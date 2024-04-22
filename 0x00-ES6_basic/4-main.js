a1cefafc18e4b6eca6f4cb994c3104ddab1d2507
Annacletter <anna.cletter12@gmail.com>
2024-04-22 22:13:29 +0200

    Task 4

--- 0x00-ES6_basic/4-main.js
+++ 0x00-ES6_basic/4-main.js
@@ -1,4 +1,4 @@
-const returnHowManyArguments from './4-rest-parameter.js';
+import returnHowManyArguments from './4-rest-parameter.js';
 
 console.log(returnHowManyArguments("one"));
 console.log(returnHowManyArguments("one", "two", 3, "4th"));
