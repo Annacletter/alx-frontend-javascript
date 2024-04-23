ef7b31b9c983df0094f0639e7ae367ee9cfc82f4
Annacletter <anna.cletter12@gmail.com>
2024-04-23 19:37:23 +0200

    Final User

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,5 +1,5 @@
-const signUpUser from './4-user-promise';
-const uploadPhoto from './5-photo-reject';
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([
