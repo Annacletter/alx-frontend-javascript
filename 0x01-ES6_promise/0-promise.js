b7980db0e6d6c314a6a15edbc7f299277e8ea02e
Annacletter <anna.cletter12@gmail.com>
2024-04-23 19:12:30 +0200

    Promise

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,13 +1,10 @@
-function getResponseFromAPI() {
-    return new Promise((resolve, reject) => {
-        // Perform asynchronous operation here (e.g., fetch data from an API)
-        
-        // For demonstration purposes, let's resolve the Promise with a dummy value
-        setTimeout(() => {
-            resolve("Dummy response from API");
-        }, 1000); // Simulate a delay of 1 second
-    });
+export default function getResponseFromAPI() {
+  const success = true;
+  return new Promise((resolve, reject) => {
+    if (success) {
+      resolve();
+    } else {
+      reject();
+    }
+  });
 }
-
-export default getResponseFromAPI;
-
