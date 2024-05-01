532a8cb7c78120cab85ce58fd42760c04bba46f3
Annacletter <111055713+Annacletter@users.noreply.github.com>
2024-05-01 20:01:50 +0200

    .eslintrc.js

---
+++ 0x04-TypeScript/task_0/.eslintrc.js
@@ -0,0 +1,12 @@
+module.exports =  {
+  parser:  '@typescript-eslint/parser',
+  extends:  [
+    'plugin:@typescript-eslint/recommended',  // Uses the recommended rules 
+  ],
+  parserOptions:  {
+    ecmaVersion:  2018,
+    sourceType:  'module',
+  },
+  rules:  {
+  },
+};
